d13ed228c6518165d3560d736ee73f5c608d4498
BCNJ <jeremy.beaucousin@cnieg.fr>
2021-09-19 14:42:53 +0200

    reordor routes

--- router.go
+++ router.go
@@ -15,11 +15,11 @@ func InitializeRouter() *mux.Router {
 	router.Methods("PUT").Path("/users/{id}").HandlerFunc(controllers.UsersUpdate)
 	router.Methods("DELETE").Path("/users/{id}").HandlerFunc(controllers.UsersDelete)
 
-	router.Methods("GET").Path("/owners/{ownerId}/properties/{propertyId}/tenants").HandlerFunc(controllers.TenantsIndex)
-	router.Methods("POST").Path("/owners/{ownerId}/properties/{propertyId}/tenants").HandlerFunc(controllers.TenantsCreate)
-	router.Methods("GET").Path("/owners/{ownerId}/properties/{propertyId}/tenants/{id}").HandlerFunc(controllers.TenantsShow)
-	router.Methods("PUT").Path("/owners/{ownerId}/properties/{propertyId}/tenants/{id}").HandlerFunc(controllers.TenantsUpdate)
-	router.Methods("DELETE").Path("/owners/{ownerId}/properties/{propertyId}/tenants/{id}").HandlerFunc(controllers.TenantsDelete)
+	router.Methods("GET").Path("/owners").HandlerFunc(controllers.OwnersIndex)
+	router.Methods("POST").Path("/owners").HandlerFunc(controllers.OwnersCreate)
+	router.Methods("GET").Path("/owners/{id}").HandlerFunc(controllers.OwnersShow)
+	router.Methods("PUT").Path("/owners/{id}").HandlerFunc(controllers.OwnersUpdate)
+	router.Methods("DELETE").Path("/owners/{id}").HandlerFunc(controllers.OwnersDelete)
 
 	router.Methods("GET").Path("/owners/{ownerId}/properties").HandlerFunc(controllers.PropertiesIndex)
 	router.Methods("POST").Path("/owners/{ownerId}/properties").HandlerFunc(controllers.PropertiesCreate)
@@ -27,11 +27,11 @@ func InitializeRouter() *mux.Router {
 	router.Methods("PUT").Path("/owners/{ownerId}/properties/{id}").HandlerFunc(controllers.PropertiesUpdate)
 	router.Methods("DELETE").Path("/owners/{ownerId}/properties/{id}").HandlerFunc(controllers.PropertiesDelete)
 
-	router.Methods("GET").Path("/owners").HandlerFunc(controllers.OwnersIndex)
-	router.Methods("POST").Path("/owners").HandlerFunc(controllers.OwnersCreate)
-	router.Methods("GET").Path("/owners/{id}").HandlerFunc(controllers.OwnersShow)
-	router.Methods("PUT").Path("/owners/{id}").HandlerFunc(controllers.OwnersUpdate)
-	router.Methods("DELETE").Path("/owners/{id}").HandlerFunc(controllers.OwnersDelete)
+	router.Methods("GET").Path("/owners/{ownerId}/properties/{propertyId}/tenants").HandlerFunc(controllers.TenantsIndex)
+	router.Methods("POST").Path("/owners/{ownerId}/properties/{propertyId}/tenants").HandlerFunc(controllers.TenantsCreate)
+	router.Methods("GET").Path("/owners/{ownerId}/properties/{propertyId}/tenants/{id}").HandlerFunc(controllers.TenantsShow)
+	router.Methods("PUT").Path("/owners/{ownerId}/properties/{propertyId}/tenants/{id}").HandlerFunc(controllers.TenantsUpdate)
+	router.Methods("DELETE").Path("/owners/{ownerId}/properties/{propertyId}/tenants/{id}").HandlerFunc(controllers.TenantsDelete)
 
 	return router
 }
